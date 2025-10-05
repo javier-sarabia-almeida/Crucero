1fa4de66487ce2bfd8a8d93cfa50634c60b8344e
Javier Sarabia <9101721@alumnos.ufv.es>
2025-10-05 22:48:22 +0200

    Init Django site (clean)

---
+++ relecloud/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from . import models
+
+admin.site.register(models.Cruise)
+admin.site.register(models.Destination)
+admin.site.register(models.InfoRequest)
+# Register your models here.
---
+++ relecloud/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+class Destination(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+    slug = models.SlugField(null=True)
+    def __str__(self) -> str:
+        return self.name
+
+class Cruise(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+    destinations = models.ManyToManyField (
+        Destination,
+        related_name='destinations'
+    )
+    def __str__(self) -> str:
+        return self.name
+
+class InfoRequest(models.Model):
+    name = models.CharField(
+        unique=True,
+        null=False,
+        blank=False,
+        max_length=50
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+    
+
